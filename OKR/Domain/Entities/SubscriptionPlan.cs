6499b60d38bf9ed6a4383451795a95c1b8bc55d0
Lyl55 <88458345+Lyl55@users.noreply.github.com>
2023-07-01 15:23:13 +0400

    added entities for all modules

---
+++ OKR/Domain/Entities/SubscriptionPlan.cs
@@ -0,0 +1,19 @@
+﻿using StorageCore.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageCore.Domain.Entities
+{
+    public class SubscriptionPlan
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public PlanType Type { get; set; }
+        public Subscription SubscriptionId { get; set; }
+        public int UserCount { get; set; }
+        public int WorkspaceCount { get; set; }
+    }
+}
