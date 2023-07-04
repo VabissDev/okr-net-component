a0ce9de617f62719003e41eb309e85ab238b6884
Lyl55 <88458345+Lyl55@users.noreply.github.com>
2023-07-04 22:17:56 +0400

    created common for all projects and added db extension

---
+++ OKR.Common/Extensions/DbExtensions.cs
@@ -0,0 +1,35 @@
+﻿using LinqToDB;
+using LinqToDB.AspNet;
+using LinqToDB.AspNet.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StorageCore.DataAccess.Linq2db;
+using StorageCore.Domain.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OKR.Common.Extensions
+{
+    public static class DbExtensions
+    {
+        public static void AddDb(this IServiceCollection services, IConfiguration configuration)
+        {
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
+            services.AddLinqToDBContext<OkrDbConnection>((provider, options) =>
+                options.UseSqlServer(connectionString)
+                .UseDefaultLogging(provider), ServiceLifetime.Scoped);
+
+            services.AddScoped<IUnitOfWork, Linq2dbUnitOfWork>();        
+
+            //settings db 
+            /*services.Configure<SqlDbOptions>(x =>
+            {
+                x.ConnectionString = connectionString;
+            });*/
+           
+        }
+    }
+}
