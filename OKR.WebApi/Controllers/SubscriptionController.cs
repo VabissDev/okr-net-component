897a9ac6503c04d263f5903231c0dab7b6414d02
Lyl55 <88458345+Lyl55@users.noreply.github.com>
2023-07-05 13:34:53 +0400

    added controller for subscription

---
+++ OKR.WebApi/Controllers/SubscriptionController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StorageCore.Domain.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OKR.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubscriptionController : ControllerBase
+    {
+        private readonly IUnitOfWork _db;
+        public SubscriptionController(IUnitOfWork db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                var subscriptions = _db.SubscriptionRepository.Get();
+
+                return Ok(subscriptions);
+            }
+            catch
+            {
+                return BadRequest("Unknown error occured");
+            }
+        }
+
+        [HttpGet]
+        [Route("GetById/{id}")]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                var subscription = _db.SubscriptionRepository.FindById(id);
+
+                if (subscription == null)
+                    return BadRequest("No subscripton found with given id");
+
+                return Ok(subscription);
+            }
+            catch
+            {
+                return BadRequest("Unknown error occured");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post(BankModel bankModel)
+        {
+            try
+            {
+                bankService.Save(bankModel);
+
+                return Ok("Success!");
+            }
+            catch
+            {
+                return BadRequest("Failed to add");
+            }
+        }
+    }
+}
---
+++ OKR.WebApi/Models/SubscriptionModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OKR.WebApi.Models
+{
+    public class SubscriptionModel
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public DateTime StartTime { get; set; }
+        [Required]
+        public DateTime EndTime { get; set; }
+        [Required]
+        public bool Active { get; set; }
+    }
+}
